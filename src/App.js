cc97f2953d17b665fe6e5b17d7681fb91ef86020
mariajbellido <mariajbellido@gmail.com>
2021-11-10 15:28:35 +0100

    Creating a Card Grid

--- src/App.js
+++ src/App.js
@@ -7,12 +7,12 @@ import './App.css'
 
 const cardImages = [
 
-  { "src": "/img/helmet-1.png"},
-  { "src": "/img/potion-1.png"},
-  { "src": "/img/ring-1.png"},
-  { "src": "/img/scroll-1.png"},
-  { "src": "/img/shield-1.png"},
-  { "src": "/img/sword-1.png"}
+  { "src": "/assets/img/helmet-1.png"},
+  { "src": "/assets/img/potion-1.png"},
+  { "src": "/assets/img/ring-1.png"},
+  { "src": "/assets/img/scroll-1.png"},
+  { "src": "/assets/img/shield-1.png"},
+  { "src": "/assets/img/sword-1.png"}
 
 ]
 
@@ -24,13 +24,13 @@ function App() {
   // Shuffle cards 
 
   const shuffleCards = () => {
-    const shuffleCards = [
+    const shuffledCards = [
       ...cardImages, ...cardImages
     ]
     .sort(() => Math.random() - 0.5)
     .map((card) => ({...card, id: Math.random()}))
 
-    setCards(shuffleCards)
+    setCards(shuffledCards)
     setTurns(0)
   }
 
@@ -38,8 +38,19 @@ function App() {
 
   return (
     <div className="App">
-      <h1>Magic Match</h1>
+      <h1>Memory Game</h1>
       <button onClick={shuffleCards}>New Game</button>
+
+      <div className="card-grid">
+        {cards.map(card => (
+          <div className="card" key={card.id}>
+              <div> 
+                <img className="front" src={card.src} alt="card front" />
+                <img className="back" src="./assets/img/cover.png" alt="card cover" />
+              </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }

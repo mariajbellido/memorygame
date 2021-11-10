76e8a435735b198375df1c9baa8c39cd866824af
mariajbellido <mariajbellido@gmail.com>
2021-11-10 16:48:17 +0100

    Comparing Card Choices

--- src/App.js
+++ src/App.js
@@ -1,62 +1,87 @@
+import React, { useState, useEffect } from 'react';
 
-import React, {useState} from 'react';
+import SingleCard from './components/SingleCard';
 
-import SingleCard from './components/SingleCard'
-
-import './App.css'
-
-
+import './App.css';
 
 const cardImages = [
-
-  { "src": "https://res.cloudinary.com/djy0sg0vl/image/upload/v1636555061/cards/scroll-1_ahduss.png"},
-  { "src": "https://res.cloudinary.com/djy0sg0vl/image/upload/v1636555061/cards/sword-1_nwmavc.png"},
-  { "src": "https://res.cloudinary.com/djy0sg0vl/image/upload/v1636555061/cards/potion-1_vrem5b.png"},
-  { "src": "https://res.cloudinary.com/djy0sg0vl/image/upload/v1636555061/cards/shield-1_gqxekf.png"},
-  { "src": "https://res.cloudinary.com/djy0sg0vl/image/upload/v1636555061/cards/ring-1_twi6vd.png"},
-  { "src": "https://res.cloudinary.com/djy0sg0vl/image/upload/v1636555061/cards/helmet-1_zosnoz.png"}
-
-]
+  {
+    src: 'https://res.cloudinary.com/djy0sg0vl/image/upload/v1636555061/cards/scroll-1_ahduss.png',
+  },
+  {
+    src: 'https://res.cloudinary.com/djy0sg0vl/image/upload/v1636555061/cards/sword-1_nwmavc.png',
+  },
+  {
+    src: 'https://res.cloudinary.com/djy0sg0vl/image/upload/v1636555061/cards/potion-1_vrem5b.png',
+  },
+  {
+    src: 'https://res.cloudinary.com/djy0sg0vl/image/upload/v1636555061/cards/shield-1_gqxekf.png',
+  },
+  {
+    src: 'https://res.cloudinary.com/djy0sg0vl/image/upload/v1636555061/cards/ring-1_twi6vd.png',
+  },
+  {
+    src: 'https://res.cloudinary.com/djy0sg0vl/image/upload/v1636555061/cards/helmet-1_zosnoz.png',
+  },
+];
 
 function App() {
+  const [cards, setCards] = useState([]);
+  const [turns, setTurns] = useState(0);
 
-  const [cards, setCards] = useState([])
-  const [turns, setTurns] = useState(0)
+  const [choiceOne, setChoiceOne] = useState(null);
+  const [choiceTwo, setChoiceTwo] = useState(null);
 
-  const [choiceOne, setChoiceOne] = useState(null)
-  const [choiceTwo, setChoiceTwo] = useState(null) 
-
-
-  // Shuffle cards 
+  // Shuffle cards
 
   const shuffleCards = () => {
-    const shuffledCards = [
-      ...cardImages, ...cardImages
-    ]
-    .sort(() => Math.random() - 0.5)
-    .map((card) => ({...card, id: Math.random()}))
+    const shuffledCards = [...cardImages, ...cardImages]
+      .sort(() => Math.random() - 0.5)
+      .map((card) => ({ ...card, id: Math.random() }));
 
-    setCards(shuffledCards)
-    setTurns(0)
-  }
+    setCards(shuffledCards);
+    setTurns(0);
+  };
 
-  // Handle choice 
-
+  // Handle choices
 
   /**
-   * If it is null, then it means that we don't have a selection for choice one. 
-   * If it evaluates to false, then it's null.  It will run setChoiceOnce. 
-   * 
+   * If it is null, then it means that we don't have a selection for choice one.
+   * If it evaluates to false, then it's null.  It will run setChoiceOnce.
+   *
    * But, if it is not null, then it has a value. If it has a value, then it means that we already have a selection made (choiceOne).
-   * In other words, it will run setChoiceTwo. 
+   * In other words, it will run setChoiceTwo.
    */
-  
+
   const handleChoice = (card) => {
-    choiceOne ? setChoiceTwo(card) : setChoiceOne(card)
-  }
+    choiceOne ? setChoiceTwo(card) : setChoiceOne(card);
+  };
+
+  
 
 
-  // console.log(cards, turns)
+  // Comparing two selected cards 
+
+  useEffect( () => {
+    if (choiceOne && choiceTwo) {
+      if (choiceOne.src === choiceTwo.src) {
+        console.log("Those cards match")
+        resetTurn()
+      } else {
+        console.log("Those cards do not match")
+        resetTurn()
+      }
+    }
+  }, [choiceOne, choiceTwo])
+
+
+  // Reset choices & increase turn
+
+  const resetTurn = () => {
+    setChoiceOne(null);
+    setChoiceTwo(null);
+    setTurns((prevTurns) => prevTurns + 1);
+  };
 
   return (
     <div className="App">
@@ -64,12 +89,12 @@ function App() {
       <button onClick={shuffleCards}>New Game</button>
 
       <div className="card-grid">
-        {cards.map(card => (
-          <SingleCard key={card.id} card={card} handleChoice={handleChoice}/>
+        {cards.map((card) => (
+          <SingleCard key={card.id} card={card} handleChoice={handleChoice} />
         ))}
       </div>
     </div>
   );
 }
 
-export default App
+export default App;
